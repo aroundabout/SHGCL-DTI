869d0836651b812da745b928548786b57d99ca92
YOGA\Dandelion <kainanyaoo@gmail.com>
2022-03-03 20:09:51 +0800

    test2

--- src/Test/tt.py
+++ src/Test/tt.py
@@ -1,4 +1,10 @@
-import scipy.sparse as sp
-import numpy as np
+import scipy.io
+import urllib.request
 
+# data_url = 'https://data.dgl.ai/dataset/ACM.mat'
+data_file_path = 'ACM.mat'
+
+# urllib.request.urlretrieve(data_url, data_file_path)
+data = scipy.io.loadmat(data_file_path)
+print(list(data.keys()))
 
